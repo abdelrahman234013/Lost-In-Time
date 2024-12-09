fb662fa5e8227952dc4490b9d2f148b0ecab5e8b
abdelrahman234013 <bido.gamal2004@gmail.com>
2024-12-09 17:31:58 +0200

    player Stats

---
+++ Lost-In-Time/Assets/All-Levels/Scripts/PlayerStats.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int health = 6;
+    public int lives = 3;
+    public float flickerTime = 0f;
+    public float flickerDuration = 0.1f;
+    private SpriteRenderer spriteRenderer;
+    public bool isImmune = false;
+    public float immuneTime = 0f;
+    public float immuneDuration = 1.5f;
+    // public TextMeshProUGUI ScoreUI;
+    // public Image healthBar;
+
+   
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.isImmune == true){
+            SpirteFlicker();
+            immuneTime = immuneTime + Time.deltaTime;
+            if(immuneTime >= this.immuneDuration){
+                this.isImmune = false;
+                this.spriteRenderer.enabled = true;
+            }
+        }
+
+        
+    }
+
+    void SpirteFlicker(){
+        if(this.flickerTime < this.flickerDuration){
+            this.flickerTime = this.flickerTime + Time.deltaTime;
+        } else{
+            spriteRenderer.enabled = !(spriteRenderer.enabled);
+            this.flickerTime = 0;
+        } 
+    }
+
+    public void TakeDamage(int damage){
+        if(this.isImmune == false){
+            this.health = this.health - damage;
+            if(this.health < 0){
+                this.health = 0;
+            }
+            if(this.lives > 0 && this.health == 0){
+                FindObjectOfType<LevelManager>().RespawnPlayer();
+                this.health = 6;
+                this.lives--;
+            } else if(this.lives == 0 && this.health == 0){
+                FindObjectOfType<LevelManager>().GameOver();
+                Destroy(this.gameObject);
+
+            }
+            else{
+                // FindObjectOfType<LevelManager>().RespawnPlayer();
+            }
+
+            Debug.Log("Player Health: " + this.health.ToString());
+            // Debug.Log("Player Lives: " + this.lives.ToString());
+        }
+
+        PlayHitReaction();
+
+        this.health = health - damage;
+        // healthBar.fillAmount = this.health/3f;
+        
+    }
+
+    void PlayHitReaction(){
+        this.isImmune = true;
+        this.immuneTime = 0f;
+    }
+}
---
+++ Lost-In-Time/Assets/Level-4/Scripts/BckgroundMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BckgroundMovement : MonoBehaviour
+{
+
+    private float startPosX, lengthX, lengthY;
+    private float startPosY;
+    public GameObject cam;
+    public float parallaxEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosX = transform.position.x;
+        startPosY = transform.position.y;
+        lengthX = GetComponent<SpriteRenderer>().bounds.size.x;
+        lengthY = GetComponent<SpriteRenderer>().bounds.size.y;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float distanceX = cam.transform.position.x * parallaxEffect;
+        float distanceY = cam.transform.position.y * parallaxEffect;
+        float movementX = cam.transform.position.x * (1 - parallaxEffect);
+        float movementY = cam.transform.position.y * (1 - parallaxEffect);
+
+        transform.position = new Vector3(startPosX + distanceX, startPosY + distanceY, transform.position.z);
+    
+    if (movementX > startPosX + lengthX )
+        {
+            startPosX += lengthX;
+
+        } else if(movementX < startPosX - lengthX)
+        {
+            startPosX -= lengthX;
+        } 
+
+         if (movementY > startPosY + lengthY )
+        {
+            startPosY += lengthY;
+
+        } else if(movementY < startPosY - lengthY)
+        {
+            startPosY -= lengthY;
+        } 
+    
+    }
+}
