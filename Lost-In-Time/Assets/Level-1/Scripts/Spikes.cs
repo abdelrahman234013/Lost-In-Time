b7e08e95d680cba94615daad1ebe2244bd983489
Hala Emad <samahala2012@gmail.com>
2024-12-12 03:07:02 +0200

    Level 1 scence 1 (music,camera)

--- Lost-In-Time/Assets/Level-1/Scripts/Spikes.cs
+++ Lost-In-Time/Assets/Level-1/Scripts/Spikes.cs
@@ -5,27 +5,25 @@ using UnityEngine;
 public class Spikes : MonoBehaviour
 {
     public int damage;
+
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
-
     }
-    void onTriggerEnter2D(Collider2D other)
-    {
-        if (other.tag == "Player")
-        {
 
-           // FindObjectOfType<LevelManager>().RespawnPlayer();
+    // Corrected the spelling of OnTriggerEnter2D
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag=="Player")  // Better to use CompareTag for efficiency
+        {
+            // Call the method that handles the player's damage
+  
             FindObjectOfType<PlayerStats>().TakeDamage(damage);
-            Debug.Log("moooot");
         }
     }
-
 }
-
