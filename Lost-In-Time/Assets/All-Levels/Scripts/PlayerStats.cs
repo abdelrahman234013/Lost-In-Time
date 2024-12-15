c61d03fa82bc7d4733989f5eae6c3e2bd6f1659a
nourKhattab-nour <nour.khattab90@gmail.com>
2024-12-15 07:14:15 -0800

    Add health bar and edit in player stats

--- Lost-In-Time/Assets/All-Levels/Scripts/PlayerStats.cs
+++ Lost-In-Time/Assets/All-Levels/Scripts/PlayerStats.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;  // Corrected this line
+using TMPro;
 
 public class PlayerStats : MonoBehaviour
 {
-    public int health = 6;
-    public int lives = 3;
+    public int health = 3; // Health for the current heart (this is per heart)
+    public int maxHealth = 3; // Maximum health per heart
+    public int lives = 3; // Total number of hearts (lives)
+    private int maxLives = 3; // Number of hearts the player can have
+
     public float flickerTime = 0f;
     public float flickerDuration = 0.1f;
     private SpriteRenderer spriteRenderer;
@@ -18,26 +21,34 @@ public class PlayerStats : MonoBehaviour
     public bool hasCapsule = false;
     private int coinsCollected = 0;
     private Animator animator;
-    public TextMeshProUGUI ScoreUI;  // Corrected this line
+
+    public TextMeshProUGUI ScoreUI;  // Score UI to show collected coins
+    public Image healthbar; // Health bar Image reference
+    public Image[] heartImages; // Array of heart images for lives (drag all heart images in the inspector)
 
     void Start()
     {
         animator = this.gameObject.GetComponent<Animator>();
         spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
+
+        // Initialize heart images based on the current lives
+        UpdateHearts();
     }
 
     void Update()
     {
-        if(this.isImmune == true){
+        if (this.isImmune == true)
+        {
             SpirteFlicker();
-            immuneTime = immuneTime + Time.deltaTime;
-            if(immuneTime >= this.immuneDuration){
+            immuneTime += Time.deltaTime;
+            if (immuneTime >= this.immuneDuration)
+            {
                 this.isImmune = false;
                 this.spriteRenderer.enabled = true;
             }
         }
 
-        ScoreUI.text = ""+coinsCollected;
+        ScoreUI.text = "" + coinsCollected;
     }
 
     void SpirteFlicker()
@@ -63,11 +74,24 @@ public class PlayerStats : MonoBehaviour
             this.health -= damage;
             if (this.health < 0) this.health = 0;
 
+            // Update health bar based on current health
+            healthbar.fillAmount = (float)health / maxHealth;  // Divide by maxHealth to dynamically adjust health bar
+
+            // Check if health in current heart is 0
             if (this.health == 0 && this.lives > 0)
             {
-                FindObjectOfType<LevelManager>().RespawnPlayer();
-                this.health = 6;
+                // Decrease the life (heart) and reset health for the next heart
                 this.lives--;
+                health = maxHealth;  // Reset health for the next heart
+
+                // Update hearts display
+                UpdateHearts();
+
+                // Respawn if there are remaining lives
+                if (this.lives > 0)
+                {
+                    FindObjectOfType<LevelManager>().RespawnPlayer();
+                }
             }
             else if (this.lives == 0 && this.health == 0)
             {
@@ -77,21 +101,21 @@ public class PlayerStats : MonoBehaviour
             Debug.Log("Player Health: " + this.health.ToString());
         }
 
-        PlayHitReaction(); 
+        PlayHitReaction();
     }
 
     void TriggerDeathAnimation()
     {
         if (animator != null)
         {
-            animator.SetTrigger("Death"); 
+            animator.SetTrigger("Death");
         }
         StartCoroutine(HandleDeath());
     }
 
     IEnumerator HandleDeath()
     {
-        yield return new WaitForSeconds(2f);  
+        yield return new WaitForSeconds(2f);
         FindObjectOfType<LevelManager>().GameOver();
         Destroy(this.gameObject);
     }
@@ -110,6 +134,23 @@ public class PlayerStats : MonoBehaviour
 
     public void CollectCoin(int coinValue)
     {
-        this.coinsCollected = this.coinsCollected + coinValue;
+        this.coinsCollected += coinValue;
+    }
+
+    // Update hearts display based on the current lives
+    private void UpdateHearts()
+    {
+        for (int i = 0; i < maxLives; i++)
+        {
+            // If the player has more than i lives, set the heart to active
+            if (i < lives)
+            {
+                heartImages[i].enabled = true;  // Show heart
+            }
+            else
+            {
+                heartImages[i].enabled = false; // Hide heart
+            }
+        }
     }
 }
